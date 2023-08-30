e29649c44cd53c3f66e376afb5d0821508d5b277
minbo2002 <minbo2002@naver.com>
2023-08-30 19:49:01 +0900

    첫 환경 설정 및 react-challenge-study-backend 연동

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+function App() {
+  const baseUrl = "http://localhost:8080";
+
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    getTodos();
+  }, []); // [] 리액트 열렸을 때 한번만 실행하는 게 하는 것!
+
+  async function getTodos() {
+    await axios // 다 받을 때까지 기다리는 것
+      .get(baseUrl + "/api/items/list")
+      .then((res) => {
+        console.log(res.data);
+        setItems(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <>
+      <h1>안녕하세요!</h1>
+      {items.map((item) => {
+        return (
+          <div key={item.id}>
+            {item.itemName} &nbsp;
+            {item.price} &nbsp;
+            {item.stockNumber} &nbsp;
+            {item.content} &nbsp;
+            {item.itemStatus}
+          </div>
+        );
+      })}
+    </>
+  );
+}
+
+export default App;
